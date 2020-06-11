4269df335de97e340e92c96255990e90766af649
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2020-06-11 11:03:56 +1000

    Add menu to choose difficulty

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,75 +1,65 @@
-"""
-Hangman Game:
-- Words to be guessed - *max word string is 10*
-- Number of Guesses
-- Guess
-    - Check guess is valid
-- main function
-    - read csv file into list
-    - filter out by min length
-    - select random word and display asterisks
-    - asks for necessary details to start
-    - takes users guess
-        - error check guess
-    - check if guess is correct
-        - display guess where letter lies
-        - or show that guess was a failure
-    - keep asking until word is uncovered or number of wrong guesses is reached
-"""
+"""Hangman Game, where user tries to guess letters of a word by Dallas Marshall."""
 
 import random
 
-NUMBER_GUESSES_ALLOWED = int(input("Please enter the number of incorrect guesses allowed: "))
-is_valid_limit = False
-while not is_valid_limit:
-    try:
-        MINIMUM_WORD_LENGTH = int(input("Please select minimum word length (1-10): "))
-        if MINIMUM_WORD_LENGTH < 1 or MINIMUM_WORD_LENGTH > 10:
-            print("Please select minimum word length between 1 and 10 letters")
-        else:
-            is_valid_limit = True
-    except ValueError:
-        print("Invalid (not an integer)")
+EASY = [3, 10]
+NORMAL = [5, 7]
+HARD = [10, 5]
+hard_mw = 10
+hard_ig = 5
+
 WORD_FILE = 'words.txt'
 
 
 def main():
     """Program is a game of hangman where user tries to guess a random word."""
 
-    guesses = []
-    letters_guessed = []
-    incorrect_guesses = 0
-
-    print("Starting Game!")
     words = read_words()
-    word = pick_word(words)
+    print("Welcome!\n")
+    menu_selection = get_valid_menu_selection()
+    while menu_selection.upper() != 'Q':
 
-    # Count number of unique letters
-    letters_in_word = []
-    for letter in word:
-        if letter not in letters_in_word:
-            letters_in_word.append(letter)
+        guesses = []
+        letters_guessed = []
+        incorrect_guesses = 0
+        game_variables = set_game_variables(menu_selection)
+        word = pick_word(words, game_variables[0])
+        print("\nStarting Game!")
 
-    while len(letters_guessed) != len(letters_in_word) and incorrect_guesses < NUMBER_GUESSES_ALLOWED:
-        crypto_word = get_crypto_word(word, guesses)
+        # Count number of unique letters
+        letters_in_word = []
+        for letter in word:
+            if letter not in letters_in_word:
+                letters_in_word.append(letter)
 
-        guess = get_valid_guess(crypto_word, letters_guessed, incorrect_guesses, guesses)
-        guesses.append(guess)
+        while len(letters_guessed) != len(letters_in_word) and incorrect_guesses < game_variables[1]:
+            crypto_word = get_crypto_word(word, guesses)
+            print(crypto_word)
+            if len(guesses) != 0:
+                print("Previous Guesses: ", end='')
+                print(*guesses, sep=',')
+                print("Guesses Remaining: {}".format((game_variables[1] - incorrect_guesses)))
 
-        if guess in word:
-            letters_guessed.append(guess)
-            print("{} is in the word\n".format(guess))
+            guess = get_valid_guess(letters_guessed, guesses)
+            guesses.append(guess)
+            if guess in word:
+                letters_guessed.append(guess)
+                print("\n{} is in the word".format(guess))
+            else:
+                incorrect_guesses += 1
+                print("\n{} is not in the word".format(guess))
+
+        if not incorrect_guesses == game_variables[1]:
+            print("You successfully guessed {}!\n".format(word))
         else:
-            incorrect_guesses += 1
-            print("{} is not in the word\n".format(guess))
-
-    if not incorrect_guesses == NUMBER_GUESSES_ALLOWED:
-        print("You successfully guessed {}!".format(word))
-    else:
-        print("Game Over!\nThe word was: {}".format(word))
+            print("Game Over!\nThe word was: {}\n".format(word))
+        menu_selection = input(
+            "Welcome!\nPlease Select from the Following Menu:\n'Q' - Quit\n'E' - Easy\n'N' - Normal\n'H' - Hard\n>>> ")
+    print("Thanks for Playing!")
 
 
 def get_crypto_word(word, guesses):
+    """Display an '*' for all un-guessed letters."""
     crypto_word = ''
     for letter in word:
         if letter not in guesses:
@@ -79,17 +69,11 @@ def get_crypto_word(word, guesses):
     return crypto_word
 
 
-def get_valid_guess(crypto_word, letters_guessed, incorrect_guesses, guesses):
-    """Display guesses and get valid user guess."""
+def get_valid_guess(letters_guessed, guesses):
+    """Get valid user guess."""
     is_valid_guess = False
     while not is_valid_guess:
-        print(crypto_word)
-        if len(guesses) != 0:
-            print("Previous Guesses: ", end='')
-            print(*guesses, sep=',')
-            print("Guesses Remaining: {}".format((NUMBER_GUESSES_ALLOWED - incorrect_guesses)))
         guess = input("Guess a letter: ")
-
         # check guess is valid
         if guess.isalpha() and guess not in letters_guessed and len(guess) == 1 and guess not in guesses:
             is_valid_guess = True
@@ -100,10 +84,9 @@ def get_valid_guess(crypto_word, letters_guessed, incorrect_guesses, guesses):
             if guess in letters_guessed:
                 print("Letter already guessed!")
             if len(guess) != 1:
-                print("Only guess one letter at a time!")
+                print("Please guess one letter at a time!")
             if guess in guesses:
                 print("You have already guessed {}".format(guess))
-            print('\n')
 
 
 def read_words():
@@ -117,14 +100,48 @@ def read_words():
     return words
 
 
-def pick_word(words):
+def pick_word(words, minimum_word_length):
     """Pick a word above minimum length"""
     long_enough_words = []
     for word in words:
-        if len(word) >= MINIMUM_WORD_LENGTH:
+        if len(word) >= minimum_word_length:
             long_enough_words.append(word)
     else:
-        return long_enough_words[random.randint(0, len(long_enough_words))]
+        return long_enough_words[random.randint(0, len(long_enough_words) - 1)]
+
+
+def set_game_variables(user_menu_selection):
+    """Set minimum word length and number of incorrect guesses allowed depending on user difficulty selection."""
+    game_variables = []
+    if user_menu_selection.upper == 'E':
+        minimum_word_length = EASY[0]
+        number_guesses_allowed = EASY[1]
+    elif user_menu_selection.upper == 'N':
+        minimum_word_length = NORMAL[0]
+        number_guesses_allowed = NORMAL[1]
+    else:
+        minimum_word_length = HARD[0]
+        number_guesses_allowed = HARD[1]
+    game_variables.append(minimum_word_length)
+    game_variables.append(number_guesses_allowed)
+    return game_variables
+
+
+def get_valid_menu_selection():
+    """Display guesses and get valid user guess."""
+    acceptable_selections = 'QENH'
+    is_valid_input = False
+    while not is_valid_input:
+        menu_selection = input(
+            "Please Select from the Following Menu:\n'Q' - Quit\n'E' - Easy\n'N' - Normal\n'H' - Hard\n>>> ")
+        try:
+            if menu_selection.upper() not in acceptable_selections or len(menu_selection) != 1:
+                print("\nInvalid Selection")
+            else:
+                is_valid_input = True
+                return menu_selection
+        except AttributeError:
+            print("\nInvalid Selection")
 
 
 if __name__ == '__main__':
