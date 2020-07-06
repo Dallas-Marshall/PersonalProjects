55d1bdf22ec9b78e8558114ffd23e8477810c50d
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2020-07-06 20:21:27 +1000

    Initial commit

---
+++ calculator/calculator_app.py
@@ -0,0 +1,8 @@
+"""More complex calculator using Calc class to practice using tkinter by Dallas Marshall."""
+from calculator.calc import Calc
+from tkinter import *
+
+root = Tk()
+root.title("Calculator")
+
+root.mainloop()
