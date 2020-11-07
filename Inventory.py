80e2dd1f13abf64e04930096e4ac1ec4cbecf778
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2020-11-07 17:23:44 +1000

    Add Inventory class

---
+++ Inventory.py
@@ -0,0 +1,24 @@
+class Inventory:
+    """Inventory class to keep track of a list of Item objects."""
+
+    def __init__(self):
+        """Initialise an Inventory."""
+        self.items = []
+
+    def __len__(self):
+        """Return number of Store objects."""
+        return len(self.items)
+
+    def list_inventory(self):
+        return self.items
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def remove_store(self, item_name):
+        """Remove an Item from the list of items."""
+        for item in self.items:
+            if item.name == item_name:
+                self.items.remove(item)
+            else:
+                return False
---
+++ test_inventory.py
@@ -0,0 +1,23 @@
+from Inventory import Inventory
+from Item import Item
+
+
+def run_tests():
+    """Test Inventory class."""
+
+    # Test empty Inventory (defaults)
+    print("Test empty Inventory:")
+    inv = Inventory()
+    assert not inv.items  # an empty list is considered False
+
+    # Test adding an Item with values
+    print("\nTest adding Item:")
+    inv.add_item(Item("Red Wool", "2 Stacks", 2))
+    inv.add_item(Item("Green Wool", "2 Stacks", 2))
+
+    # Test list_inventory
+    for item in inv.list_inventory():
+        print(item)
+
+
+run_tests()
