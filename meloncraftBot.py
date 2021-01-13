1c50ccdb37e8a85428e70dbb44cbf35ad2391bb3
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-01-13 23:00:51 +1000

    update !help to include command specific help

--- meloncraftBot.py
+++ meloncraftBot.py
@@ -99,31 +99,50 @@ async def add_item(ctx, *, details):
     else:
         embed = discord.Embed(title="Error: Please use the following format", color=0xff0000)
         embed.add_field(name="!add_item <Store_Name> : <Item_Name> : <Quantity> : <Cost_in_Diamonds>",
-                        value="\te.g. !add_item Big W : Iron : 2 Stacks : 1", inline=False)
-        embed.add_field(name="Iron", value="2 Stacks / 1D")
+                        value="e.g. !add_item All Australian Wool : Red Wool : 2 Stacks : 1", inline=False)
+        embed.add_field(name="Red Wool", value="2 Stacks / 1D")
     await ctx.send(embed=embed)
 
 
 @client.command(pass_context=True)
-async def help(ctx):
-    embed = discord.Embed(title="Meloncraft Bot Help", color=0xff0000)
-    embed.add_field(name="!stores", value="Returns list of all stores.", inline=False)
-    # add_store
-    embed.add_field(
-        name="!add_store <Store_Name> : <x-Coord> : <y-Coord> : <Description>"
-             "\n\te.g. !add_store Big W : 0 : 502 : Lowest Prices Everyday",
-        value="Adds Store to list of stores.", inline=False)
-    # remove_store
-    embed.add_field(name="!remove_store <Store_Name>\n\te.g. !remove_store Big W",
-                    value="Removes Store from list of stores.", inline=False)
-    # list_inventory
-    embed.add_field(name="!list_inventory <Store_Name>\n\te.g. !list_inventory Big W",
-                    value="Returns list of items for sale at Store.", inline=False)
-    # add_item
-    embed.add_field(
-        name="!add_item <Store_Name> : <Item_Name> : <Quantity> : <Cost_in_Diamonds>"
-             "\n\te.g. !add_item Big W : Iron : 2 Stacks : 1",
-        value="Adds Item to list of items for sale at Store.", inline=False)
+async def help(ctx, *args):
+    embed = discord.Embed(title="Meloncraft Bot Help:", color=0xff0000)
+    if len(args) == 0:  # command not specified
+        embed.add_field(name="!stores", value="Returns list of all stores.", inline=False)
+        embed.add_field(name="!add_store", value="Adds Store to list of stores.", inline=False)
+        embed.add_field(name="!remove_store", value="Removes Store from list of stores.", inline=False)
+        embed.add_field(name="!list_inventory", value="Returns list of items for sale at Store.", inline=False)
+        embed.add_field(name="!add_item", value="Adds an Item to list of items for sale at Store.", inline=False)
+        embed.add_field(name="!remove_item", value="Removes an Item to list of items for sale at Store.", inline=False)
+        embed.add_field(name="For detailed command specific help:", value="Use !help <command_name> e.g. !help stores",
+                        inline=False)
+    elif len(args) == 1:  # command specified
+        requested_command_name = args[0]
+        if requested_command_name == "stores":
+            embed.add_field(name="!stores", value="Returns list of all stores.", inline=False)
+        elif requested_command_name == "add_store":
+            embed.add_field(name="!add_store <Store_Name> : <x-Coord> : <y-Coord> : <Description>",
+                            value="e.g. !add_store All Australian Wool : 300 : 400 : All your wool Needs!",
+                            inline=False)
+        elif requested_command_name == "remove_store":
+            embed.add_field(name="!remove_store <Store_Name>",
+                            value="e.g. !remove_store All Australian Wool", inline=False)
+        elif requested_command_name == "list_inventory":
+            embed.add_field(name="!list_inventory <Store_Name>",
+                            value="e.g. !list_inventory All Australian Wool", inline=False)
+        elif requested_command_name == "add_item":
+            embed.add_field(name="!add_item <Store_Name> : <Item_Name> : <Quantity> : <Cost_in_Diamonds>",
+                            value="e.g. !add_item All Australian Wool : Red Wool : 2 Stacks : 1", inline=False)
+        elif requested_command_name == "remove_item":
+            embed.add_field(name="!remove_item <Store_Name> : <Item_Name>",
+                            value="\te.g. !remove_item All Australian Wool : Red Wool", inline=False)
+        else:  # unknown command
+            embed = discord.Embed(title="Error", color=0xff0000)
+            embed.add_field(name=f'Command \'{requested_command_name}\', could not be found.',
+                            value="Use !help for a list of commands.", inline=False)
+    elif len(args) > 1:  # too many inputs
+        embed = discord.Embed(title="Error: Please use the following format", color=0xff0000)
+        embed.add_field(name="!help <command>", value="e.g. !help list_inventory", inline=False)
     await ctx.send(embed=embed)
 
 
@@ -141,8 +160,6 @@ async def remove_item(ctx, *, details):
         for store in catalogue.list_stores():
             if store.name.lower() == store_name.lower():
                 is_valid_store_name = True
-
-                is_removed = False
                 # Attempt to remove Item from store inventory
                 is_removed = store.inventory.remove_item(item_name)
                 if not is_removed:
@@ -151,13 +168,12 @@ async def remove_item(ctx, *, details):
                     # Update save file
                     catalogue.save_stores(PATH_TO_DATA_FILE)
                     embed = discord.Embed(title='Item Removed Successfully', color=0xff0000)
-
         if not is_valid_store_name:
             embed = discord.Embed(title=f'Store Name: \'{store_name}\' cannot be found', color=0xff0000)
     else:
         embed = discord.Embed(title="Error: Please use the following format", color=0xff0000)
         embed.add_field(name="!remove_item <Store_Name> : <Item_Name>",
-                        value="\te.g. !remove_item Big W : Iron", inline=False)
+                        value="\te.g. !remove_item All Australian Wool : Red Wool", inline=False)
     await ctx.send(embed=embed)
 
 
