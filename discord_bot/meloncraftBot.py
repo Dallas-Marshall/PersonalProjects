3cbff21c401dc607c6d819cf4bb79f244db62bbe
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-02-20 16:35:08 +1000

    rename .list_inventory to .stock and add error checking

--- discord_bot/meloncraftBot.py
+++ discord_bot/meloncraftBot.py
@@ -45,21 +45,28 @@ async def add_store(ctx, *, details):
         catalogue.load_stores(PATH_TO_DATA_FILE)
         catalogue.add_store(Store(store_name, store_location_x, store_location_z, store_description))
         catalogue.save_stores(PATH_TO_DATA_FILE)
+        await ctx.send("Store added successfully!")
         await stores(ctx)
 
 
-@client.command(name='list_inventory', aliases=["list", "List", "Inv", "inv", "list_inv", "List_Inv", 'stock', 'Stock'])
+@client.command(name='stock', aliases=["list", "List", "Inv", "inv", "list_inv", "List_Inv", 'list_inventory', 'Stock'])
 async def list_inventory(ctx, *, store_name):
     catalogue = Catalogue()
     catalogue.load_stores(PATH_TO_DATA_FILE)
+    is_valid_store = False
     for store in catalogue.list_stores():
         if store.name.lower() == store_name.lower():
+            is_valid_store = True
             embed = discord.Embed(title=f'{store.name}', color=13424046)
             if store.inventory.__len__() == 0:
                 embed.add_field(name="Currently has no Stock!", value="Please check back later.", inline=False)
-            for item in store.inventory.list_items():
-                embed.add_field(name=item.name, value=f'{item.quantity} / {item.cost}D', inline=False)
+            else:
+                for item in store.inventory.list_items():
+                    embed.add_field(name=item.name, value=f'{item.quantity} / {item.cost}D', inline=False)
             await ctx.send(embed=embed)
+    if not is_valid_store:
+        await ctx.send(f'\'{store_name}\' could not be found.')
+        await stores(ctx)
 
 
 @client.command(name='remove_store', aliases=['removestore', 'remove_s'])
@@ -74,9 +81,8 @@ async def remove_store(ctx, *, store_name):
     else:
         # Update save file
         catalogue.save_stores(PATH_TO_DATA_FILE)
-        embed = discord.Embed(title='Store Removed Successfully', color=13424046)
-        await ctx.send(embed=embed)
-    await stores(ctx)
+        await ctx.send("Store removed successfully!")
+        await stores(ctx)
 
 
 @client.command(name='sell', aliases=['add_item', 'additem', 'Sell'])
@@ -122,7 +128,7 @@ async def help(ctx, *args):
         embed.add_field(name=".stores", value="Returns list of all stores.", inline=False)
         embed.add_field(name=".add_store", value="Adds Store to list of stores.", inline=False)
         embed.add_field(name=".remove_store", value="Removes Store from list of stores.", inline=False)
-        embed.add_field(name=".list_inventory", value="Returns list of items for sale at Store.", inline=False)
+        embed.add_field(name=".stock", value="Returns list of items for sale at Store.", inline=False)
         embed.add_field(name=".sell", value="Adds an Item to list of items for sale at Store.", inline=False)
         embed.add_field(name=".remove_item", value="Removes an Item to list of items for sale at Store.", inline=False)
         embed.add_field(name=".update_item", value="Updates an existing Item in Stores inventory.", inline=False)
@@ -142,9 +148,9 @@ async def help(ctx, *args):
         elif requested_command_name == "remove_store":
             embed.add_field(name=".remove_store <Store_Name>",
                             value="e.g. .remove_store All Australian Wool", inline=False)
-        elif requested_command_name == "list_inventory":
-            embed.add_field(name=".list_inventory <Store_Name>",
-                            value="e.g. .list_inventory All Australian Wool", inline=False)
+        elif requested_command_name == "stock":
+            embed.add_field(name=".stock <Store_Name>",
+                            value="e.g. .stock All Australian Wool", inline=False)
         elif requested_command_name == "sell":
             embed.add_field(name=".sell <Store_Name> : <Item_Name> : <Quantity> : <Cost_in_Diamonds>",
                             value="e.g. .add_item All Australian Wool : Red Wool : 2 Stacks : 1", inline=False)
