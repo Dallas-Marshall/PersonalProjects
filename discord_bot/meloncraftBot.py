6f29d2ce3805e27bcd321c6d466c8900fd914200
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-06-28 23:47:24 +1000

    Tidy up code versions by setting one standard for naming

--- discord_bot/meloncraftBot.py
+++ discord_bot/meloncraftBot.py
@@ -6,13 +6,13 @@ from catalogue import Catalogue
 from store import Store
 from similar_search import calculate_similarity
 
-PATH_TO_DATA_FILE = "store_saves.csv"
+PATH_TO_DATA_FILE = 'store_saves.csv'
 
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 client = commands.Bot(command_prefix='.', intents=intents)
 
 # Remove default help command
-client.remove_command("help")
+client.remove_command('help')
 
 
 @client.event
@@ -24,7 +24,7 @@ async def on_ready():
 async def stores(ctx):
     catalogue = Catalogue()
     catalogue.load_stores(PATH_TO_DATA_FILE)
-    embed = discord.Embed(title="MelonCraft Stores", color=13424046)
+    embed = discord.Embed(title='MelonCraft Stores', color=13424046)
     for store in catalogue.list_stores():
         embed.add_field(name=store.name, value=f'{store.description} ({store.location_x}, {store.location_z})',
                         inline=False)
@@ -35,7 +35,7 @@ async def stores(ctx):
 async def add_store(ctx, *, details):
     store_details = details.split(':')
     if len(store_details) != 4:
-        await ctx.send("Error: Incorrect Formatting")
+        await ctx.send('Error: Incorrect Formatting')
         await help(ctx, 'add_store')
     else:
         store_name = store_details[0].strip()
@@ -46,11 +46,11 @@ async def add_store(ctx, *, details):
         catalogue.load_stores(PATH_TO_DATA_FILE)
         catalogue.add_store(Store(store_name, store_location_x, store_location_z, store_description))
         catalogue.save_stores(PATH_TO_DATA_FILE)
-        await ctx.send("Store added successfully!")
+        await ctx.send('Store added successfully!')
         await stores(ctx)
 
 
-@client.command(name='stock', aliases=["list", "List", "Inv", "inv", "list_inv", "List_Inv", 'list_inventory', 'Stock'])
+@client.command(name='stock', aliases=['list', 'List', 'Inv', 'inv', 'list_inv', 'List_Inv', 'list_inventory', 'Stock'])
 async def list_inventory(ctx, *, store_name):
     catalogue = Catalogue()
     catalogue.load_stores(PATH_TO_DATA_FILE)
@@ -60,7 +60,7 @@ async def list_inventory(ctx, *, store_name):
             is_valid_store = True
             embed = discord.Embed(title=f'{store.name}', color=13424046)
             if store.inventory.__len__() == 0:
-                embed.add_field(name="Currently has no Stock!", value="Please check back later.", inline=False)
+                embed.add_field(name='Currently has no Stock!', value='Please check back later.', inline=False)
             else:
                 for item in store.inventory.list_items():
                     embed.add_field(name=item.name, value=f'{item.quantity} / {item.cost}D', inline=False)
@@ -82,7 +82,7 @@ async def remove_store(ctx, *, store_name):
     else:
         # Update save file
         catalogue.save_stores(PATH_TO_DATA_FILE)
-        await ctx.send("Store removed successfully!")
+        await ctx.send('Store removed successfully!')
         await stores(ctx)
 
 
@@ -115,73 +115,73 @@ async def add_item(ctx, *, details):
         if not is_valid_store_name:
             embed = discord.Embed(title=f'Store: \'{store_name}\' cannot be found', color=13424046)
     else:
-        embed = discord.Embed(title="Error: Please use the following format", color=13424046)
-        embed.add_field(name=".sell <Store_Name> : <Item_Name> : <Quantity> : <Cost_in_Diamonds>",
-                        value="e.g. .sell All Australian Wool : Red Wool : 2 Stacks : 1", inline=False)
-        embed.add_field(name="Red Wool", value="2 Stacks / 1D")
+        embed = discord.Embed(title='Error: Please use the following format', color=13424046)
+        embed.add_field(name='.sell <Store_Name> : <Item_Name> : <Quantity> : <Cost_in_Diamonds>',
+                        value='e.g. .sell All Australian Wool : Red Wool : 2 Stacks : 1', inline=False)
+        embed.add_field(name='Red Wool', value='2 Stacks / 1D')
     await ctx.send(embed=embed)
 
 
 @client.command(pass_context=True)
 async def help(ctx, *args):
-    embed = discord.Embed(title="Meloncraft Commands List:", color=13424046)
+    embed = discord.Embed(title='Meloncraft Commands List:', color=13424046)
     if len(args) == 0:  # Command not specified
-        embed.add_field(name=".stores", value="Returns list of all stores.", inline=False)
-        embed.add_field(name=".add_store", value="Adds Store to list of stores.", inline=False)
-        embed.add_field(name=".remove_store", value="Removes Store from list of stores.", inline=False)
-        embed.add_field(name=".stock", value="Returns list of items for sale at Store.", inline=False)
-        embed.add_field(name=".sell", value="Adds an Item to list of items for sale at Store.", inline=False)
-        embed.add_field(name=".remove_item", value="Removes an Item to list of items for sale at Store.", inline=False)
-        embed.add_field(name=".update_item", value="Updates an existing Item in Stores inventory.", inline=False)
-        embed.add_field(name=".update_store", value="Updates an existing Store in Catalogue.", inline=False)
-        embed.add_field(name=".find", value="Returns list of stores if any with specified item in stock.")
-        embed.add_field(name=".bot_info", value="Gives Brief description of Bot.", inline=False)
-        embed.add_field(name="For detailed command specific help:", value="Use .help <command_name> e.g. .help stores",
+        embed.add_field(name='.stores', value='Returns list of all stores.', inline=False)
+        embed.add_field(name='.add_store', value='Adds Store to list of stores.', inline=False)
+        embed.add_field(name='.remove_store', value='Removes Store from list of stores.', inline=False)
+        embed.add_field(name='.stock', value='Returns list of items for sale at Store.', inline=False)
+        embed.add_field(name='.sell', value='Adds an Item to list of items for sale at Store.', inline=False)
+        embed.add_field(name='.remove_item', value='Removes an Item to list of items for sale at Store.', inline=False)
+        embed.add_field(name='.update_item', value='Updates an existing Item in Stores inventory.', inline=False)
+        embed.add_field(name='.update_store', value='Updates an existing Store in Catalogue.', inline=False)
+        embed.add_field(name='.find', value='Returns list of stores if any with specified item in stock.')
+        embed.add_field(name='.bot_info', value='Gives Brief description of Bot.', inline=False)
+        embed.add_field(name='For detailed command specific help:', value='Use .help <command_name> e.g. .help stores',
                         inline=False)
     elif len(args) == 1:  # Command specified
-        embed = discord.Embed(title="Meloncraft Commands Help:", color=13424046)
+        embed = discord.Embed(title='Meloncraft Commands Help:', color=13424046)
         requested_command_name = args[0]
-        if requested_command_name == "stores":
-            embed.add_field(name=".stores", value="Returns list of all stores.", inline=False)
-        elif requested_command_name == "add_store":
-            embed.add_field(name=".add_store <Store_Name> : <x_Coord> : <z_Coord> : <Description>",
-                            value="e.g. .add_store All Australian Wool : 300 : 400 : All your wool Needs!",
+        if requested_command_name == 'stores':
+            embed.add_field(name='.stores', value='Returns list of all stores.', inline=False)
+        elif requested_command_name == 'add_store':
+            embed.add_field(name='.add_store <Store_Name> : <x_Coord> : <z_Coord> : <Description>',
+                            value='e.g. .add_store All Australian Wool : 300 : 400 : All your wool Needs!',
                             inline=False)
-        elif requested_command_name == "remove_store":
-            embed.add_field(name=".remove_store <Store_Name>",
-                            value="e.g. .remove_store All Australian Wool", inline=False)
-        elif requested_command_name == "stock":
-            embed.add_field(name=".stock <Store_Name>",
-                            value="e.g. .stock All Australian Wool", inline=False)
-        elif requested_command_name == "sell":
-            embed.add_field(name=".sell <Store_Name> : <Item_Name> : <Quantity> : <Cost_in_Diamonds>",
-                            value="e.g. .add_item All Australian Wool : Red Wool : 2 Stacks : 1", inline=False)
-        elif requested_command_name == "remove_item":
-            embed.add_field(name=".remove_item <Store_Name> : <Item_Name>",
-                            value="\te.g. .remove_item All Australian Wool : Red Wool", inline=False)
-        elif requested_command_name == "find":
-            embed.add_field(name=".find <Item_Name>",
-                            value="\te.g. .find Red Wool", inline=False)
-        elif requested_command_name == "update_item":
+        elif requested_command_name == 'remove_store':
+            embed.add_field(name='.remove_store <Store_Name>',
+                            value='e.g. .remove_store All Australian Wool', inline=False)
+        elif requested_command_name == 'stock':
+            embed.add_field(name='.stock <Store_Name>',
+                            value='e.g. .stock All Australian Wool', inline=False)
+        elif requested_command_name == 'sell':
+            embed.add_field(name='.sell <Store_Name> : <Item_Name> : <Quantity> : <Cost_in_Diamonds>',
+                            value='e.g. .add_item All Australian Wool : Red Wool : 2 Stacks : 1', inline=False)
+        elif requested_command_name == 'remove_item':
+            embed.add_field(name='.remove_item <Store_Name> : <Item_Name>',
+                            value='\te.g. .remove_item All Australian Wool : Red Wool', inline=False)
+        elif requested_command_name == 'find':
+            embed.add_field(name='.find <Item_Name>',
+                            value='\te.g. .find Red Wool', inline=False)
+        elif requested_command_name == 'update_item':
             embed.add_field(
-                name=".update_item <Store_Name> : <Old_Item_Name> : <New_Item_Name> : <New_Item_Quantity> : "
-                     "<New_Item_Cost>",
-                value="\te.g. .update_item All Australian Wool : Red Wool : Blue Wool : 2 Stacks : 1", inline=False)
-        elif requested_command_name == "update_store":
+                name='.update_item <Store_Name> : <Old_Item_Name> : <New_Item_Name> : <New_Item_Quantity> : '
+                     '<New_Item_Cost>',
+                value='\te.g. .update_item All Australian Wool : Red Wool : Blue Wool : 2 Stacks : 1', inline=False)
+        elif requested_command_name == 'update_store':
             embed.add_field(
-                name=".update_store <Old_Store_Name> : <New_Store_name> : <New_X_Coord> : <New_Z_Coord> : "
-                     "<New_Description>",
-                value="\te.g. .update_store All Australian Wool : non-Australian Wool : 50 : -234 : Cheap Wool",
+                name='.update_store <Old_Store_Name> : <New_Store_name> : <New_X_Coord> : <New_Z_Coord> : '
+                     '<New_Description>',
+                value='\te.g. .update_store All Australian Wool : non-Australian Wool : 50 : -234 : Cheap Wool',
                 inline=False)
-        elif requested_command_name == "bot_info":
-            embed.add_field(name=".bot_info", value="Gives Brief description of Bot.", inline=False)
+        elif requested_command_name == 'bot_info':
+            embed.add_field(name='.bot_info', value='Gives Brief description of Bot.', inline=False)
         else:  # Unknown command
-            embed = discord.Embed(title="Error", color=13424046)
+            embed = discord.Embed(title='Error', color=13424046)
             embed.add_field(name=f'Command \'{requested_command_name}\', could not be found.',
-                            value="Use .help for a list of commands.", inline=False)
+                            value='Use .help for a list of commands.', inline=False)
     elif len(args) > 1:  # Too many inputs
-        embed = discord.Embed(title="Error: Please use the following format", color=13424046)
-        embed.add_field(name=".help <command>", value="e.g. .help list_inventory", inline=False)
+        embed = discord.Embed(title='Error: Please use the following format', color=13424046)
+        embed.add_field(name='.help <command>', value='e.g. .help list_inventory', inline=False)
     await ctx.send(embed=embed)
 
 
@@ -211,9 +211,9 @@ async def remove_item(ctx, *, details):
         if not is_valid_store_name:
             embed = discord.Embed(title=f'Store Name: \'{store_name}\' cannot be found', color=13424046)
     else:
-        embed = discord.Embed(title="Error: Please use the following format", color=13424046)
-        embed.add_field(name=".remove_item <Store_Name> : <Item_Name>",
-                        value="\te.g. .remove_item All Australian Wool : Red Wool", inline=False)
+        embed = discord.Embed(title='Error: Please use the following format', color=13424046)
+        embed.add_field(name='.remove_item <Store_Name> : <Item_Name>',
+                        value='\te.g. .remove_item All Australian Wool : Red Wool', inline=False)
     await ctx.send(embed=embed)
 
 
@@ -251,11 +251,11 @@ async def update_item(ctx, *, details):
         if not is_valid_store_name:
             embed = discord.Embed(title=f'Store: \'{store_name}\' cannot be found', color=13424046)
     else:
-        embed = discord.Embed(title="Error: Please use the following format", color=13424046)
-        embed.add_field(name=".update_item <Store_Name> : <Old_Item_Name> : <New_Item_Name> : <New_Item_Quantity> : "
-                             "<New_Item_Cost>",
-                        value="\t.update_item All Australian Wool : Red Wool : Blue Wool : 2 Stacks : 1", inline=False)
-        embed.add_field(name="Blue Wool", value="2 Stacks / 1D")
+        embed = discord.Embed(title='Error: Please use the following format', color=13424046)
+        embed.add_field(name='.update_item <Store_Name> : <Old_Item_Name> : <New_Item_Name> : <New_Item_Quantity> : '
+                             '<New_Item_Cost>',
+                        value='\t.update_item All Australian Wool : Red Wool : Blue Wool : 2 Stacks : 1', inline=False)
+        embed.add_field(name='Blue Wool', value='2 Stacks / 1D')
     await ctx.send(embed=embed)
 
 
@@ -268,7 +268,7 @@ async def find_item(ctx, *, item_name):
     embed = discord.Embed(title=f'\'{item_name}\' was found in stock at:', color=13424046)
     for store in catalogue.list_stores():
         for item in store.inventory.list_items():
-            if calculate_similarity(item_name.lower(), item.name.lower()) > .5: # Matching item
+            if calculate_similarity(item_name.lower(), item.name.lower()) > .5:  # Matching item
                 number_of_instances_found += 1
                 embed.add_field(name=f'{store.name} - ({store.location_x}, {store.location_z})',
                                 value=f'{item.name} - {item.quantity} / {item.cost}D', inline=False)
@@ -281,12 +281,12 @@ async def find_item(ctx, *, item_name):
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        embed = discord.Embed(title="Incorrect Command Usage", color=13424046)
-        embed.add_field(name="For Command Specific Help Use", value=".help <command> e.g. .help list_inventory",
+        embed = discord.Embed(title='Incorrect Command Usage', color=13424046)
+        embed.add_field(name='For Command Specific Help Use', value='.help <command> e.g. .help list_inventory',
                         inline=False)
         await ctx.send(embed=embed)
     elif isinstance(error, commands.CommandNotFound):
-        await ctx.send("Error: Invalid Command")
+        await ctx.send('Error: Invalid Command')
         await help(ctx)
     else:
         raise error
@@ -295,7 +295,6 @@ async def on_command_error(ctx, error):
 @client.command(name='update_store', aliases=['edit_store', 'editstore', 'updatestore'])
 async def update_store(ctx, *, details):
     store_details = details.split(':')
-    embed = discord.Embed()
     if len(store_details) == 5:  # Correct number of arguments provided
         old_store_name = store_details[0].strip()
         new_store_name = store_details[1].strip()
@@ -321,19 +320,19 @@ async def update_store(ctx, *, details):
             await ctx.send(embed=embed)
         await stores(ctx)
     else:
-        embed = discord.Embed(title="Error: Please use the following format", color=13424046)
+        embed = discord.Embed(title='Error: Please use the following format', color=13424046)
         await ctx.send(embed=embed)
-        await help(ctx, "update_store")
+        await help(ctx, 'update_store')
 
 
-@client.command(name="bot_info")
+@client.command(name='bot_info')
 async def bot_info(ctx):
-    embed = discord.Embed(title="Honeydew Discord Bot", color=13424046)
-    embed.add_field(name="Bot Creator", value="Made by mobpuncher1", inline=False)
-    embed.add_field(name="Bot Purpose",
-                    value="Display items for sale on mc.meloncraft.net as well as provide ability to find an item to "
-                          "purchase", inline=False)
-    embed.add_field(name="Bot Usage", value="Type .help to see the commands to use Honeydew")
+    embed = discord.Embed(title='Honeydew Discord Bot', color=13424046)
+    embed.add_field(name='Bot Creator', value='Made by mobpuncher1', inline=False)
+    embed.add_field(name='Bot Purpose',
+                    value='Display items for sale on mc.meloncraft.net as well as provide ability to find an item to '
+                          'purchase', inline=False)
+    embed.add_field(name='Bot Usage', value='Type .help to see the commands to use Honeydew')
     await ctx.send(embed=embed)
 
 
